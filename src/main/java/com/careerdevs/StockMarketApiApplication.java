6d992d8ae5a507ed4b27016d82a00c0e98bec0da
Nathan1006 <nrpetit@outlook.com>
2021-12-02 17:54:02 -0500

    Initial commit

---
+++ src/main/java/com/careerdevs/StockMarketApiApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMarketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockMarketApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/careerdevs/StockMarketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMarketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
